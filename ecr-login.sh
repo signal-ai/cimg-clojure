7cb19e1de4f55b659e224c82e5b247d5b3a7394c
Charlie Briggs <charliebriggs@gmail.com>
2024-05-31 16:54:51 +0000

    Use us-east-1

--- ecr-login.sh
+++ ecr-login.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Logging into Amazon Elastic Container Registry"
-aws ecr get-login-password \
+aws ecr get-login-password --region us-east-1 \
 | docker login \
     --username AWS \
-    --password-stdin 166248964328.dkr.ecr.eu-west-1.amazonaws.com
+    --password-stdin 166248964328.dkr.ecr.us-east-1.amazonaws.com
--- push-images.sh
+++ push-images.sh
@@ -1,39 +1,39 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 set -eo pipefail
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-8.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-8.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-8.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-8.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-8.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-8.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-11.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-11.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-11.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-11.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-11.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-11.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-17.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-17.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-17.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-17.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-17.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-17.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0
-docker tag 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3
-docker tag 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-node
-docker tag 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-node 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-node
-docker tag 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-node 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-node
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-browsers
-docker tag 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-browsers 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-browsers
-docker tag 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-browsers 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11-browsers
-docker push 166248964328.dkr.ecr.eu-west-1.amazonaws.com/cimg-clojure:1.11.3-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-8.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-8.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-8.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-8.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-8.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-8.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-11.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-11.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-11.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-11.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-11.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-11.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-17.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-17.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-17.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-17.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-17.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-17.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0
+docker tag 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3
+docker tag 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-node
+docker tag 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-node 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-node
+docker tag 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-node 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-node
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-browsers
+docker tag 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-openjdk-21.0-browsers 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-browsers
+docker tag 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-openjdk-21.0-browsers 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11-browsers
+docker push 166248964328.dkr.ecr.us-east-1.amazonaws.com/cimg-clojure:1.11.3-browsers
